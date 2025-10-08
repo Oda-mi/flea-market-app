7e7ac6afc59e3ba47ed4d4f3ae47aec2f20b2df0
oda mi <oda2025mi@gmail.com>
2025-10-08 09:17:47 +0900

    リレーション追加

--- src/app/Models/Item.php
+++ src/app/Models/Item.php
@@ -32,7 +32,7 @@ class Item extends Model
 
     public function categories()
     {
-        return $this->belongsToMany(Category::class);
+        return $this->belongsToMany(Category::class, 'category_item');
     }
 
     public function purchase()
