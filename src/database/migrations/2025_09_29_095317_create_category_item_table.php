daff5c32de69852560bd7282534a877449aa02f2
oda mi <oda2025mi@gmail.com>
2025-10-13 14:44:42 +0900

    中間テーブルのストレージエンジンにInnoDBを指定 追加

--- src/database/migrations/2025_09_29_095317_create_category_item_table.php
+++ src/database/migrations/2025_09_29_095317_create_category_item_table.php
@@ -14,6 +14,7 @@ class CreateCategoryItemTable extends Migration
     public function up()
     {
         Schema::create('category_item', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->id();
             $table->foreignId('item_id')->constrained()->cascadeOnDelete();
             $table->foreignId('category_id')->constrained()->cascadeOnDelete();
