466a71a56b7553e24fd29ec37d4f1239dc801e00
oda mi <oda2025mi@gmail.com>
2025-10-21 10:35:44 +0900

    メール認証機能追加（Fortify使用）

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -26,8 +26,8 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //ログアウト後のリダイレクト
         $this->app->instance(LogoutResponse::class, new class implements LogoutResponse {
+
         public function toResponse($request)
         {
             return redirect('/login');
@@ -42,37 +42,35 @@ class FortifyServiceProvider extends ServiceProvider
     {
         Fortify::createUsersUsing(CreateNewUser::class);
 
-        /*会員登録画面の表示*/
         Fortify::registerView(function () {
             return view ('auth.register');
         });
 
-        /*ログイン画面の表示*/
         Fortify::loginView(function () {
             return view('auth.login');
         });
 
-         // ログイン後のリダイレクト
-        $this->app->instance(LoginResponse::class, new class implements LoginResponse {
-        public function toResponse($request)
-        {
-            return redirect('/');
-        }
+        Fortify::verifyEmailView(function () {
+            return view('auth.verify-email');
         });
 
-        // 登録後のリダイレクト
+
+        // 登録後のリダイレクト先
         $this->app->instance(RegisterResponse::class, new class implements RegisterResponse {
         public function toResponse($request)
         {
-            return redirect('/mypage/profile');
+            return redirect()->route('verification.notice');
         }
         });
 
-        //ログイン失敗時エラーメッセージ
+        //ログイン処理
         Fortify::authenticateUsing(function ($request) {
             $user = User::where('email',$request->email)->first();
 
             if ($user && Hash::check($request->password, $user->password)) {
+                if (!$user->hasVerifiedEmail()) {
+                    session(['redirect_to_verify' => true]);
+                }
                 return $user;
             }
             throw ValidationException::withMessages([
@@ -80,16 +78,26 @@ class FortifyServiceProvider extends ServiceProvider
             ]);
         });
 
-        //ログインのタイムリミット設定
+         // ログイン後のリダイレクト先
+        $this->app->instance(LoginResponse::class, new class implements LoginResponse {
+        public function toResponse($request)
+        {
+            if (session('redirect_to_verify')) {
+                session()->forget('redirect_to_verify');
+                return redirect()->route('verification.notice');
+            }
+            return redirect('/');
+        }
+        });
+
+
         RateLimiter::for('login', function (Request $request) {
             $email = (string) $request->email;
 
             return Limit::perMinute(10)->by($email . $request->ip());
         });
 
-        // カスタム LoginRequest を Fortify にバインド
+
         $this->app->bind(FortifyLoginRequest::class, LoginRequest::class);
-
     }
-
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\PurchaseController;
 use App\Http\Controllers\MypageController;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 
-//誰でも見れる//
 // トップページ
 Route::get('/', [ItemController::class, 'index'])->name('items.index');
 // 商品詳細
@@ -17,44 +18,55 @@ Route::get('/item/{item_id}',[ItemController::class, 'show'])->name('items.show'
 //ログイン必須 ミドルウェア設定
 Route::middleware('auth')->group(function () {
 
-//マイページ
-Route::get('/mypage',[MypageController::class,'index'])->name('mypage.index');
+    //メール認証誘導画面
+    Route::get('/email/verify', function () {
+        return view ('auth.verify-email');
+    })->name('verification.notice');
 
-//マイページ編集
-Route::get('/mypage/profile',[MypageController::class,'profile'])->name('mypage.profile');
+    //メール認証メール送信
+    Route::post('/email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
 
-//マイページ更新
-Route::put('/mypage/profile',[MypageController::class,'update'])->name('mypage.update');
+        return back();
+    })->middleware(['auth','throttle:6,1'])->name('verification.send');
 
-//商品購入
-Route::get('/purchase/{item_id}',[PurchaseController::class, 'index'])->name('purchase.index');
+    //メール認証リンククリック後の処理
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
 
-Route::post('/purchase/{item_id}', [PurchaseController::class, 'store'])->name('purchase.store');
+        return redirect()->route('mypage.profile');
+    })->middleware(['auth', 'signed'])->name('verification.verify');
 
-//住所変更画面
-Route::get('/purchase/address/{item_id}',[PurchaseController::class,'address'])->name('purchase.address');
+    //マイページ
+    Route::get('/mypage',[MypageController::class,'index'])->name('mypage.index');
 
-Route::put('/purchase/address/{item_id}',[PurchaseController::class,'updateAddress'])->name('purchase.updateAddress');
+    //マイページ編集
+    Route::get('/mypage/profile',[MypageController::class,'profile'])->name('mypage.profile');
 
-//出品
-Route::get('/sell', [ItemController::class, 'sell'])->name('items.sell');
+    //マイページ更新
+    Route::put('/mypage/profile',[MypageController::class,'update'])->name('mypage.update');
 
-Route::post('sell',[ItemController::class, 'store'])->name('items.store');
+    //商品購入
+    Route::get('/purchase/{item_id}',[PurchaseController::class, 'index'])->name('purchase.index');
 
-//商品詳細画面コメント送信
-Route::post('item/{item_id}/comment',[ItemController::class,'storeComment'])->name('comment.store');
+    Route::post('/purchase/{item_id}', [PurchaseController::class, 'store'])->name('purchase.store');
 
-//商品詳細画面いいね機能
-Route::post('/items/{item}/favorite', [ItemController::class, 'toggleFavorite'])->name('items.toggleFavorite');
+    //住所変更画面
+    Route::get('/purchase/address/{item_id}',[PurchaseController::class,'address'])->name('purchase.address');
+
+    Route::put('/purchase/address/{item_id}',[PurchaseController::class,'updateAddress'])->name('purchase.updateAddress');
+
+    //出品
+    Route::get('/sell', [ItemController::class, 'sell'])->name('items.sell');
+
+    Route::post('sell',[ItemController::class, 'store'])->name('items.store');
+
+    //商品詳細画面コメント送信
+    Route::post('item/{item_id}/comment',[ItemController::class,'storeComment'])->name('comment.store');
+
+    //商品詳細画面いいね機能
+    Route::post('/items/{item}/favorite', [ItemController::class, 'toggleFavorite'])->name('items.toggleFavorite');
 
 });
 
 
-
-
-
-
-// メール認証誘導画面
-Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-});
