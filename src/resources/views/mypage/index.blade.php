231dd811d3f3d67b295d93680cc85bee93f71853
oda mi <oda2025mi@gmail.com>
2026-01-05 21:00:16 +0900

    取引評価の星マーク追加+ナビタブに「取引中の商品」タブ追加

--- src/resources/views/mypage/index.blade.php
+++ src/resources/views/mypage/index.blade.php
@@ -12,7 +12,16 @@
             <img src="{{ $user->profile_image ? asset('storage/'.$user->profile_image) : asset('/images/default.png') }}" alt="プロフィール画面" class="image">
         </div>
         <div class="profile__info">
-            <p class="profile__name">{{ $user->name }}</p>
+            <div class="profile__name-area">
+                <p class="profile__name">{{ $user->name }}</p>
+                <div class="profile__rating">
+                    <span class="star star--active">★</span>
+                    <span class="star star--active">★</span>
+                    <span class="star star--active">★</span>
+                    <span class="star star--active">★</span>
+                    <span class="star star--inactive">★</span>
+                </div>
+            </div>
         </div>
         <div class="profile__edit">
             <a href="/mypage/profile" class="profile__edit-btn">プロフィールを編集</a>
@@ -22,8 +31,9 @@
 
     <div class="nav">
         <div class="nav__tabs">
-        <a href="{{ route('mypage.index',['page' => 'sell']) }}" class="nav__tab {{ $tab === 'sell' ? 'nav__tab--active' : '' }}">出品した商品</a>
-        <a href="{{ route('mypage.index', ['page' => 'buy']) }}" class="nav__tab {{ $tab === 'buy' ? 'nav__tab--active' : '' }}">購入した商品</a>
+            <a href="{{ route('mypage.index',['page' => 'sell']) }}" class="nav__tab {{ $tab === 'sell' ? 'nav__tab--active' : '' }}">出品した商品</a>
+            <a href="{{ route('mypage.index', ['page' => 'buy']) }}" class="nav__tab {{ $tab === 'buy' ? 'nav__tab--active' : '' }}">購入した商品</a>
+            <a href="{{ route('mypage.index', ['page' => 'trading']) }}" class="nav__tab {{ $tab === 'trading' ? 'nav__tab--active' : '' }}">取引中の商品</a>
         </div>
     </div>
 
@@ -36,9 +46,11 @@
             </div>
             @empty
                 @if ($tab === 'sell')
-                <p class="product__empty-message">出品した商品はありません</p>
-                @else ($tab === 'buy')
-                <p class="product__empty-message">購入した商品はありません</p>
+                    <p class="product__empty-message">出品した商品はありません</p>
+                @elseif ($tab === 'buy')
+                    <p class="product__empty-message">購入した商品はありません</p>
+                @elseif ($tab === 'trading')
+                    <p class="product__empty-message">取引中の商品はありません</p>
                 @endif
             @endforelse
         </div>
