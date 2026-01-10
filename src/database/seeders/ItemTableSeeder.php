7096e58abaf3a4eb0023f748ee9fc90b8ddc427f
oda mi <oda2025mi@gmail.com>
2026-01-10 12:24:37 +0900

    Itemダミーデータ　itemsにcategories紐付け

--- src/database/seeders/ItemTableSeeder.php
+++ src/database/seeders/ItemTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Item;
+use App\Models\Category;
+
 
 class ItemTableSeeder extends Seeder
 {
@@ -14,101 +16,142 @@ class ItemTableSeeder extends Seeder
      */
     public function run()
     {
+
+        $categories = Category::all()->keyBy('name');
+
         $items = [
             [
-                'name' => '腕時計',
-                'price' => 15000,
-                'brand' => 'Rolax',
-                'description' => 'スタイリッシュなデザインのメンズ腕時計',
-                'img_url' => 'clock.jpg',
-                'condition_id' => 1,
-                'user_id' => 1,
+                'item' => [
+                    'name'         => '腕時計',
+                    'price'        => 15000,
+                    'brand'        => 'Rolax',
+                    'description'  => 'スタイリッシュなデザインのメンズ腕時計',
+                    'img_url'      => 'clock.jpg',
+                    'condition_id' => 1,
+                    'user_id'      => 1,
+                ],
+                'categories' => ['メンズ', 'アクセサリー'],
             ],
             [
-                'name' => 'HDD',
-                'price' => 5000,
-                'brand' => '西芝',
-                'description' => '高速で信頼性の高いハードディスク',
-                'img_url' => 'HDD.jpg',
-                'condition_id' => 2,
-                'user_id' => 1,
+                'item' => [
+                    'name'         => 'HDD',
+                    'price'        => 5000,
+                    'brand'        => '西芝',
+                    'description'  => '高速で信頼性の高いハードディスク',
+                    'img_url'      => 'HDD.jpg',
+                    'condition_id' => 2,
+                    'user_id'      => 1,
+                ],
+                'categories' => ['家電'],
             ],
             [
-                'name' => '玉ねぎ３束',
-                'price' => 300,
-                'brand' => null,
-                'description' => '新鮮な玉ねぎ３束のセット',
-                'img_url' => 'onion.jpg',
-                'condition_id' => 3,
-                'user_id' => 1,
+                'item' => [
+                    'name'         => '玉ねぎ３束',
+                    'price'        => 300,
+                    'brand'        => null,
+                    'description'  => '新鮮な玉ねぎ３束のセット',
+                    'img_url'      => 'onion.jpg',
+                    'condition_id' => 3,
+                    'user_id'      => 1,
+                ],
+                'categories' => ['キッチン'],
             ],
             [
-                'name' => '革靴',
-                'price' => 4000,
-                'brand' => null,
-                'description' => 'クラッシックなデザインの革靴',
-                'img_url' => 'shoes.jpg',
-                'condition_id' => 4,
-                'user_id' => 1,
+                'item' => [
+                    'name'         => '革靴',
+                    'price'        => 4000,
+                    'brand'        => null,
+                    'description'  => 'クラッシックなデザインの革靴',
+                    'img_url'      => 'shoes.jpg',
+                    'condition_id' => 4,
+                    'user_id'      => 1,
+                ],
+                'categories' => ['ファッション', 'メンズ'],
             ],
             [
-                'name' => 'ノートPC',
-                'price' => 45000,
-                'brand' => null,
-                'description' => '高性能なノートパソコン',
-                'img_url' => 'PC.jpg',
-                'condition_id' => 1,
-                'user_id' => 1,
+                'item' => [
+                    'name'         => 'ノートPC',
+                    'price'        => 45000,
+                    'brand'        => null,
+                    'description'  => '高性能なノートパソコン',
+                    'img_url'      => 'PC.jpg',
+                    'condition_id' => 1,
+                    'user_id'      => 1,
+                ],
+                'categories' => ['家電'],
             ],
             [
-                'name' => 'マイク',
-                'price' => 8000,
-                'brand' => null,
-                'description' => '高音質のレコーディング用マイク',
-                'img_url' => 'mic.jpg',
-                'condition_id' => 2,
-                'user_id' => 2,
+                'item' => [
+                    'name'         => 'マイク',
+                    'price'        => 8000,
+                    'brand'        => null,
+                    'description'  => '高音質のレコーディング用マイク',
+                    'img_url'      => 'mic.jpg',
+                    'condition_id' => 2,
+                    'user_id'      => 2,
+                ],
+                'categories' => ['家電'],
             ],
             [
-                'name' => 'ショルダーバッグ',
-                'price' => 3500,
-                'brand' => null,
-                'description' => 'おしゃれなショルダーバッグ',
-                'img_url' => 'bag.jpg',
-                'condition_id' => 3,
-                'user_id' => 2,
+                'item' => [
+                    'name'         => 'ショルダーバッグ',
+                    'price'        => 3500,
+                    'brand'        => null,
+                    'description'  => 'おしゃれなショルダーバッグ',
+                    'img_url'      => 'bag.jpg',
+                    'condition_id' => 3,
+                    'user_id'      => 2,
+                ],
+                'categories' => ['ファッション', 'レディース'],
             ],
             [
-                'name' => 'タンブラー',
-                'price' => 500,
-                'brand' => null,
-                'description' => '使いやすいタンブラー',
-                'img_url' => 'tumbler.jpg',
-                'condition_id' => 4,
-                'user_id' => 2,
+                'item' => [
+                    'name'         => 'タンブラー',
+                    'price'        => 500,
+                    'brand'        => null,
+                    'description'  => '使いやすいタンブラー',
+                    'img_url'      => 'tumbler.jpg',
+                    'condition_id' => 4,
+                    'user_id'      => 2,
+                ],
+                'categories' => ['キッチン'],
             ],
             [
-                'name' => 'コーヒーミル',
-                'price' => 4000,
-                'brand' => 'Starbacks',
-                'description' => '手動のコーヒーミル',
-                'img_url' => 'coffee.jpg',
-                'condition_id' => 1,
-                'user_id' => 2,
+                'item' => [
+                    'name'         => 'コーヒーミル',
+                    'price'        => 4000,
+                    'brand'        => 'Starbacks',
+                    'description'  => '手動のコーヒーミル',
+                    'img_url'      => 'coffee.jpg',
+                    'condition_id' => 1,
+                    'user_id'      => 2,
+                ],
+                'categories' => ['キッチン'],
             ],
             [
-                'name' => 'メイクセット',
-                'price' => 2500,
-                'brand' => null,
-                'description' => '便利なメイクアップセット',
-                'img_url' => 'make.jpg',
-                'condition_id' => 2,
-                'user_id' => 2,
+                'item' => [
+                    'name'         => 'メイクセット',
+                    'price'        => 2500,
+                    'brand'        => null,
+                    'description'  => '便利なメイクアップセット',
+                    'img_url'      => 'make.jpg',
+                    'condition_id' => 2,
+                    'user_id'      => 2,
+                ],
+                'categories' => ['コスメ'],
             ],
         ];
 
-        foreach($items as $item) {
-            Item::create($item);
+        foreach($items as $data) {
+            $item = Item::create($data['item']);
+
+            $categoryIds = [];
+            foreach ($data['categories'] as $categoryName) {
+                if (isset($categories[$categoryName])) {
+                    $categoryIds[] = $categories[$categoryName]->id;
+                }
+            }
+            $item->categories()->attach($categoryIds);
         }
     }
 }
